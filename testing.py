f3aaa5ce548dcf753ee0e28c3ce586b68618607e
katya1tch <echikireva@fmschool72.ru>
2020-01-29 14:40:09 +0500

    start window

--- testing.py
+++ testing.py
@@ -1,8 +1,21 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Kate')
+import pygame
+
+pygame.init()
+size = width, height = 1100, 700
+screen = pygame.display.set_mode(size)
 
 
-if __name__ == '__main__':
-    main()
+def draw():
+    pass
+
+
+pygame.display.flip()
+running = True
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    draw()
+    pygame.display.flip()
+    clock.tick(24)
